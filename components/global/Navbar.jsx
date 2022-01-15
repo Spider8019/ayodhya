e7f28b5ee427f19e6411f46a9ce445d9afbaed39
Spider8019 <spider8019official@gmail.com>
2022-01-15 21:30:50 +0530

    basic setup with tailwind and next translate

---
+++ components/global/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+                <h1>District Ayodhya</h1>
+                <p>JAI SHRI RAM</p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
