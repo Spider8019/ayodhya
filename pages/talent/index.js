dd9b6d1e940c9a45fce206f04e5e9c9b12b1d69c
Spider8019 <spider8019official@gmail.com>
2022-02-17 23:19:35 +0530

    product other cateogry talent page

--- pages/talent/index.js
+++ pages/talent/index.js
@@ -24,10 +24,11 @@ export default function SimpleSlider() {
     };
     const {data:top10,error:top10Error}=useSWR('top10Talents',()=>getTop10Talents({category:"artworks"}))
     const {data:top10Photos,error:top10PhotosError}=useSWR('top10TalentsPhotos',()=>getTop10Talents({category:"photography"}))
+    const {data:top10Others,error:top10OthersError}=useSWR('top10TalentsOthers',()=>getTop10Talents({category:"others"}))
 
-    if(!top10 || !top10Photos)
+    if(!top10 || !top10Photos || top10Others)
        return <h1>loading</h1>
-    if(top10Error || top10PhotosError){
+    if(top10Error || top10PhotosError || top10OthersError){
         return <p>Error while fetching</p>
     }
     return (
@@ -135,6 +136,55 @@ export default function SimpleSlider() {
                 </Slider>
             </div>
         </div>
+        <h2 className="text-2xl my-8">Others</h2>
+            <div className={styles.slickContainer}>
+                <Slider {...settings}
+                >
+                {
+                    top10.map((item,key)=>{
+                        return(
+                            <div 
+                                key={key}
+                                className={`${styles.slickBox} `} >
+                                <div className="m-2 bg-white rounded p-2 relative"
+                                    style={{boxShadow:"1px 1px 10px rgba(0,0,0,0.164)"}}
+                                >
+                                    <div className="absolute left-4 top-4 z-30">
+                                        <GetProfile id={item.createdBy} />
+                                    </div>
+                                    <Image 
+                                    src={item.imageList[0]}
+                                    alt="Image Talents"
+                                    layout="responsive"
+                                    objectFit="cover"
+                                    height="1"
+                                    width="1"
+                                    />
+                                    <div className="flex justify-between p-2">
+                                        <div>
+                                            <p>{item.about.slice(0,25)}{item.about.length>25 && "..."}</p>
+                                            <p className="text-xs">{dateFormat(item.createdAt,"fullDate")}</p>
+                                        </div>
+                                        <div className="flex flex-col pl-2">
+                                            <div className="flex items-center justify-end">
+                                                <p className='pr-2'>{millify(item.view)}</p>
+                                                <FilterTiltShiftIcon/>
+                                            </div>
+                                            <div className="flex items-center justify-end">
+                                                <p className='pr-2'>{millify(item.length.likedBy)}</p>
+                                                <FavoriteBorderIcon/>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                </Slider>
+            </div>
+
       </>
     );
 }
