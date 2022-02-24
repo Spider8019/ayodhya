798e59913d4e09606c4851fc952e753d4f721b9f
Spider8019 <spider8019official@gmail.com>
2022-02-24 08:25:50 +0530

    addlogs

--- components/adSense/ad1.js
+++ components/adSense/ad1.js
@@ -17,7 +17,6 @@ export default function Adsense() {
 
   return (
     <ins className="adsbygoogle"
-     style={{display:"block",textAlign:"center"}}
      data-ad-layout="in-article"
      data-ad-format="fluid"
      data-ad-client="ca-pub-5871634443514718"
