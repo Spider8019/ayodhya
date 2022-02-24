f66dd94b5c2df05a764ad140a45825de1c72d834
Spider8019 <spider8019official@gmail.com>
2022-02-24 06:50:51 +0530

    google adsense 3

--- pages/_document.js
+++ pages/_document.js
@@ -2,21 +2,12 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
 
-  componentWillMount = () => {
-    var ads = document.getElementsByClassName("adsbygoogle").length;
-    for (var i = 0; i < ads; i++) {
-      try {
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      } catch (e) { }
-    }
- }
-
   render() {
     return (
       <Html lang="en">
         <Head>
           <meta name='keywords' content='spider8019 aman pratap singh meta ikshvaku ayodhya ram talent bhajans' />
-          {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
           <script dangerouslySetInnerHTML={{
             __html: `
             (adsbygoogle = window.adsbygoogle || []).push({
@@ -24,7 +15,7 @@ class MyDocument extends Document {
                 enable_page_level_ads: true
                 });
                 `,
-            }} /> */}
+            }} />
 
         </Head>
         <body>
