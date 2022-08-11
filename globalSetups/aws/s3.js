cc2928dfee7c56099a681b8bec1d1997f412e5e0
spider8019 <spider8019official@gmail.com>
2022-08-12 03:12:27 +0530

    aws configuration

--- globalSetups/aws/s3.js
+++ globalSetups/aws/s3.js
@@ -1,43 +1,43 @@
 import AWS from "aws-sdk"
-import {nanoid} from "nanoid"
+import { nanoid } from "nanoid"
 import _ from "lodash"
 
 AWS.config.update({
-    accessKeyId:"AKIAYRGQ44ZVC6BGZZ63",
-    secretAccessKey:"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"
+    accessKeyId: "AKIAYRGQ44ZVBPYEUFW5",
+    secretAccessKey: "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"
 })
 
 const myBucket = new AWS.S3({
-    params: { Bucket:"ikshvaku-s3"},
+    params: { Bucket: "ikshvakubucket" },
     region: "ap-south-1",
 })
 
-export const uploadObject = async({file,filename},callback) =>{
+export const uploadObject = async ({ file, filename }, callback) => {
 
-    const params={
+    const params = {
         ACL: 'public-read',
         Body: file,
-        Bucket:"ikshvaku-s3",
+        Bucket: "ikshvakubucket",
         Key: filename
     }
     await myBucket.upload(params)
-    .on('httpUploadProgress', (evt) => {
-        return (Math.round((evt.loaded / evt.total) * 100))
-    })
-    .send((err,data) => {
-        callback(err,data)
-    })
+        .on('httpUploadProgress', (evt) => {
+            return (Math.round((evt.loaded / evt.total) * 100))
+        })
+        .send((err, data) => {
+            callback(err, data)
+        })
 }
-export const deleteObject = async({url},callback) =>{
+export const deleteObject = async ({ url }, callback) => {
 
     // https://ikshvaku-s3.s3.ap-south-1.amazonaws.com/spider80194bbiek3i8FEXKjl_h6R33b7df2cb6b36abfc7c7b93d1670d7946a.jpg
-    const params={
-        Bucket:"ikshvaku-s3",
-        Key:url.slice(48,)
+    const params = {
+        Bucket: "ikshvakubucket",
+        Key: url.slice(48,)
     }
     console.log(params)
     await myBucket.deleteObject(params)
-    .send((err,data) => {
-        callback(err,data)
-    })
+        .send((err, data) => {
+            callback(err, data)
+        })
 }
