d1b70e615df9bf8062bea93687fd6e93760a7eda
spider8019 <spider8019official@gmail.com>
2022-08-12 15:37:40 +0530

    final changes

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextTranslate = require('next-translate')
 module.exports = {
     ...nextTranslate(),
     images: {
-        domains: ['images.sadhguru.org','thetalentedworld.net','gumlet.assettype.com','akm-img-a-in.tosshub.com','openweathermap.org','images.unsplash.com','www.trueshayari.in','ikshvaku-s3.s3.ap-south-1.amazonaws.com','i.pinimg.com'],
-      },
+        domains: ['images.sadhguru.org', 'thetalentedworld.net', 'gumlet.assettype.com', 'akm-img-a-in.tosshub.com', 'openweathermap.org', 'images.unsplash.com', 'www.trueshayari.in', 'ikshvakubucket.s3.ap-south-1.amazonaws.com', 'ikshvaku-s3.s3.ap-south-1.amazonaws.com', 'i.pinimg.com'],
+    },
     devIndicators: {
         buildActivity: false
     }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,6 +19,7 @@ export default NextAuth({
           password: credentials.password,
         };
         const user = await loginProfile(payload)
+        console.log(payload, user)
         if (user.status === 200) {
           return user.data.user
         }
@@ -45,6 +46,7 @@ export default NextAuth({
       return session;
     },
     async redirect({ url, baseUrl }) {
+      console.log(url, baseUrl)
       return baseUrl
     }
   },
