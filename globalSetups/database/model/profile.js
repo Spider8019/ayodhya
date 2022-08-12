73ff3ebd8d45cfa5f544b9ac92ec2adb6363015a
spider8019 <spider8019official@gmail.com>
2022-08-12 13:40:48 +0530

    update

--- globalSetups/database/model/profile.js
+++ globalSetups/database/model/profile.js
@@ -2,56 +2,56 @@ import mongoose from "mongoose"
 import bcrypt from "bcrypt"
 
 const ProfileSchema = new mongoose.Schema({
-    name:{
-          type:String,
-          required:[true, 'Name required'],
-        },
-    about:{
-          type:String,
+    name: {
+        type: String,
+        required: [true, 'Name required'],
     },
-    isVerified:{
-          type:Boolean,
-          default:false
+    about: {
+        type: String,
     },
-    isDeveloper:{
-          type:Boolean,
-          default:false
+    isVerified: {
+        type: Boolean,
+        default: false
     },
-    email:{
-          type:String,
-          required:[true, 'Email required'],
-          unique:[true,'Not Unique'],
-          lowercase:true,
-          validate: {
-            validator: function(v) {
+    isDeveloper: {
+        type: Boolean,
+        default: false
+    },
+    email: {
+        type: String,
+        required: [true, 'Email required'],
+        unique: [true, 'Not Unique'],
+        lowercase: true,
+        validate: {
+            validator: function (v) {
                 const reg = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
                 return reg.test(v)
             },
             message: props => `${props.value} is not a valid email!`
-          },
+        },
     },
-    password:{
-             type:String,
-             required:[true,'Password Required']
-            },
-    image:{
-        type:Number,
-        default:0,
+    password: {
+        type: String,
+        required: [true, 'Password Required']
     },
-    availableImages:{
-            type:Array,
-            "default":['https://ikshvaku-s3.s3.ap-south-1.amazonaws.com/static/images.png','https://ikshvaku-s3.s3.ap-south-1.amazonaws.com/static/1a3ea756b8ec93542184c8bb4631205e.jpg','/static/preview.png']
+    image: {
+        type: Number,
+        default: 0,
     },
-    coverImage:{
-        type:String,
-        default:"https://images.unsplash.com/photo-1645280403333-3775178fc8c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60"
+    availableImages: {
+        type: Array,
+        "default": ['https://ikshvakubucket.s3.ap-south-1.amazonaws.com/Untitled+design.png', 'https://ikshvakubucket.s3.ap-south-1.amazonaws.com/Untitled+design.png', '/static/preview.png']
     },
-    aadhar:String,
+    coverImage: {
+        type: String,
+        default: "https://ikshvakubucket.s3.ap-south-1.amazonaws.com/Beige+Minimalist+Happy+Chuseok+Korean+Mid+Autumn+Moon+Festival+Twitter+Header.png"
+    },
+    aadhar: String,
 })
 
-ProfileSchema.pre("save",async function(next) {
-    if (this.isModified('password')){
-        this.password= await bcrypt.hash(this.password, 10)
+ProfileSchema.pre("save", async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10)
     }
     next();
 });
